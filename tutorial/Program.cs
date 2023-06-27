a14d6393b5b2d53bdc6132fe299bb38a7c8e959c
Alestorm <aleaguirre_666@hotmail.com>
2023-06-27 14:07:45 -0400

    agregacion para listar departaments y proyectos por empleado

--- tutorial/Program.cs
+++ tutorial/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using tutorial.services;
 
 namespace tutorial
 {
@@ -11,10 +14,24 @@ namespace tutorial
     {
         static void Main(string[] args)
         {
-            StreamWriter writer = new StreamWriter("prueba.txt");
 
-            writer.WriteLine("Hola mundo");
-            writer.Close();
+            EmployeeService employeeService = new EmployeeService();
+            
+            foreach(var emp in employeeService.GetEmployeeProjects())
+            {
+                Console.WriteLine("Project name:");
+                Console.WriteLine(emp.Project.project_name);
+                Console.WriteLine($"Employee: {emp.Employee.emp_fname} {emp.Employee.emp_lname}");
+                
+            }
+
+            Console.WriteLine("DEPARTMENTS");
+            DepartmentService departmentService = new DepartmentService();
+            foreach (var dep in departmentService.GetAll())
+            {
+                Console.WriteLine($"ID: {dep.dept_no}");
+                Console.WriteLine($"Deparment name: {dep.dept_name} - Location: {dep.location}");
+            }
 
             Console.ReadKey();
         }
